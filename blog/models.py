270f212b799e6de8cf514304a46780e3e16cdae7
Praveensagar <praveensagar1604@gmail.com>
2026-03-20 20:00:00 +0530

    Initial Django project

---
+++ blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils.text import slugify
+
+    
+# Catagory
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+# Good
+    def __str__(self):
+        return self.name
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    img_url = models.URLField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def safe(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+class AboutUs(models.Model):
+    content= models.TextField()
+
+    
